45ddc29adce1b361d27c313fc200ba9c8369c60b
Vladimir Biriukov <wowas18@icloud.com>
2025-12-30 12:53:49 +0300

    Update app.py

--- wallet_project/wallet/core/app.py
+++ wallet_project/wallet/core/app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
 wallet_app = FastAPI(
-    title="Бэкенд кошелька на FastAPI+PostgreSQL " \
-    "Репозиторий github.com/vovsya/fastapiwallet"
+    title="Бэкенд кошелька на FastAPI+PostgreSQL\nРепозиторий github.com/vovsya/fastapiwallet"
     )
